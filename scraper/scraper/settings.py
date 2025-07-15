c0ef02a94aac2a348862eedc8e24e60862555dc0
Reza Taheri <94985322+RezaTaheri01@users.noreply.github.com>
2025-07-15 20:12:32 +0330

    Add sorl.thumbnail and static files config
    Added 'sorl.thumbnail' to INSTALLED_APPS for image thumbnail support. Configured STATICFILES_DIRS to include the main static directory, improving static file management.

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'games',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),  # search main file (not apps)
+    BASE_DIR / "static"
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
